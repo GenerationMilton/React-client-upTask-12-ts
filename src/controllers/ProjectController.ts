567786a53c350b2d0cc60d2a0142d8b4eba636bb
Milton Munoz <livemilton@gmail.com>
2026-02-04 22:06:04 -0500

    uptask-backend :sparkles: Creating the first projects

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -1,9 +1,23 @@
 import type { Request, Response } from 'express';
+import Project from '../models/Project';
 
 export class ProjectController {
 
     //execute the controler method calling from routes
     static getAllProjects = async(req: Request, res: Response) =>{
         res.send('Todos los proyectos');
+    }
+
+     static createProject = async(req: Request, res: Response) =>{
+        console.log(req.body)
+        const project = new Project(req.body);
+
+        try {
+            await project.save()
+            res.send('Proyecto Creado Correctamente')
+        } catch (error) {
+            console.log(error)
+        }
+
     }
 }
--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -4,6 +4,7 @@ import { ProjectController } from '../controllers/ProjectController'
 const router = Router()
 
 //Http method to call
-router.get('/', ProjectController.getAllProjects )
+router.get('/', ProjectController.getAllProjects)
+router.post('/', ProjectController.createProject)
 
 export default router
