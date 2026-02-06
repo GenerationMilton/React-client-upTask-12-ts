dd029f49211494101f2b94f3a6e597d89803c58b
Milton Munoz <livemilton@gmail.com>
2026-02-05 22:07:53 -0500

    uptask-backend :sparkles: Adding validation in projects server

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -1,10 +1,23 @@
 import { Router } from 'express'
 import { ProjectController } from '../controllers/ProjectController'
+import { body } from 'express-validator'
+import { handleInputErrors } from '../middleware/validation'
 
 const router = Router()
 
 //Http method to call
-router.get('/', ProjectController.getAllProjects)
-router.post('/', ProjectController.createProject)
+router.get('/',ProjectController.getAllProjects
+
+)
+router.post('/', 
+    body('projectName')
+        .notEmpty().withMessage('El nombre del Proyecto es Obligatorio'),
+    body('clientName')
+        .notEmpty().withMessage('El nombre del Cliente es Obligatorio'),
+    body('description')
+        .notEmpty().withMessage('La descripción del Proyecto es Obligatoria'),
+    handleInputErrors,    
+    ProjectController.createProject
+)
 
 export default router
