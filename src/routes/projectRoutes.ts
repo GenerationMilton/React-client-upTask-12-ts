e0ecc9189538b33ea0ba7ea3248ee6e3d29a219b
Milton Munoz <livemilton@gmail.com>
2026-02-08 11:23:28 -0500

    uptask-backend :sparkles: Adding Validation to Tasks

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -48,6 +48,11 @@ router.delete('/:id',
 /** Routes for tasks */
 router.post('/:projectId/tasks',
     validateProjectExists,
+    body('name')
+        .notEmpty().withMessage('El nombre de la tarea es Obligatorio'),
+    body('description')
+        .notEmpty().withMessage('La descripción de la tarea es obligatoria'),
+    handleInputErrors,
     TaskController.createTask
 )
 
