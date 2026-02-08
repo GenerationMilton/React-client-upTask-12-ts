1d5fca0b9ad1173725cae66cbbd5de1427b9c3a1
Milton Munoz <livemilton@gmail.com>
2026-02-08 15:42:03 -0500

    uptask-backend :sparkles: Creating the Endpoint to Update Task Statuses

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -88,4 +88,15 @@ router.delete('/:projectId/tasks/:taskId',
     TaskController.deleteTask
 )
 
+
+/** Routes for tasks state by Status */
+router.post('/:projectId/tasks/:taskId/status',
+    param('taskId').isMongoId().withMessage('ID no válido'),
+    body('status')
+        .notEmpty().withMessage('El estado es obligatorio'),
+    handleInputErrors,
+    TaskController.updateStatus
+)
+
+
 export default router
