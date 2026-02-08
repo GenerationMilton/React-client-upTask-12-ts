359cea83440b90a4904bc9fff5b2f6874f58ade7
Milton Munoz <livemilton@gmail.com>
2026-02-08 12:01:59 -0500

    uptask-backend :sparkles: Retrieving Tasks from a Project

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -27,7 +27,7 @@ export class ProjectController {
     const { id } = req.params;
     console.log(id);
     try {
-      const project = await Project.findById(id);
+      const project = await Project.findById(id).populate('tasks')
       if (!project) {
         const error = new Error("Proyecto no encontrado");
         return res.status(404).json({ error: error.message });
