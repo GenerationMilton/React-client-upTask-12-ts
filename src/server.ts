b3a4c36c64c4b3a83bd281dd3fbcd4acd6701717
Milton Munoz <livemilton@gmail.com>
2026-02-17 22:10:27 -0500

    uptask-backend :sparkles: ReactQueryDevtools and install morgan

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import cors from 'cors';
 import { corsConfig } from './config/cors';
 import { connectDB } from './config/db';
 import projectRoutes from './routes/projectRoutes';
-
+import morgan from 'morgan';
 
 dotenv.config();
 connectDB();
@@ -12,7 +12,12 @@ connectDB();
 const app = express();
 app.use(cors(corsConfig))
 
+//Logging
+app.use(morgan('dev'))
+
+// Leer datos de formularios
 app.use(express.json())
+
 // Routes
 app.use('/api/projects', projectRoutes)
 
