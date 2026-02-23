eea84d515ba0f74759ff822749c440b5544820f6
Milton Munoz <livemilton@gmail.com>
2026-02-23 18:51:36 -0500

    uptask-backend :sparkles: Authentication - Creating the controller and validation

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,9 +1,24 @@
 import { Router } from "express";
+import { AuthController } from "../controllers/AuthController";
+import { body } from "express-validator";
+import { handleInputErrors } from "../middleware/validation";
 
 const router = Router()
 
-router.get('/', (req, res) => {
-    res.send('desde /api/auth')
-})
+router.post('/create-account', 
+    body('name')
+        .notEmpty().withMessage('El nombre no puede ir vacio'),
+    body('password')
+        .isLength({min: 8}).withMessage('El password es muy corto, minimo 8 caracteres'), 
+    body('password_confirmation').custom((value, {req})=> {
+        if(value !== req.body.password){
+            throw new Error('Los Password no son iguales')
+        }
+        return true
+    }),
+    body('email')
+        .notEmpty().withMessage('E-mail no válido'),
+    handleInputErrors,
+    AuthController.createAccount)
 
 export default router
