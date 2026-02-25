bcb0877d1ce3997301044b0daee618244771a4bc
Milton Munoz <livemilton@gmail.com>
2026-02-24 22:38:02 -0500

    uptask-backend :sparkles: Authentication - Sending The token to the registered user's email address

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -3,8 +3,7 @@ import User from "../models/User"
 import { hashPassword } from "../utils/auth"
 import Token from "../models/Token"
 import { generateToken } from "../utils/token"
-import { transporter } from "../config/nodemailer"
-
+import { AuthEmail } from "../emails/AuthEmail"
 
 export class AuthController {
 
@@ -33,12 +32,10 @@ export class AuthController {
             token.user = user._id
 
             // Generar el email
-            await transporter.sendMail({
-                from: 'UpTask <admin@uptask.com>',
-                to: user.email,
-                subject: 'UpTask - Confirma tu cuenta',
-                text: 'UpTask - Confirma tu cuenta',
-                html: `<p>Probando e-mail</p>`
+            AuthEmail.sendConfirmationEmail({
+                email: user.email,
+                name: user.email,
+                token: token.token
             })
   
             await Promise.allSettled([user.save(), token.save()])
