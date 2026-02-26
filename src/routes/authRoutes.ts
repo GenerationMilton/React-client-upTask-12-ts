05d3c98376935a5c42c236ffe712e1a27acc6ad8
Milton Munoz <livemilton@gmail.com>
2026-02-25 19:31:01 -0500

    uptask-backend :sparkles: Authentication - Creating the Login method and validate if user exists in DB

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -28,4 +28,14 @@ router.post('/confirm-account',
     AuthController.confirmAccount
 )
 
+router.post('/login',
+    body('email')
+        .isEmail().withMessage('E-mail no válido'),
+    body('password')
+        .notEmpty().withMessage('El password no puede ir vacio'),
+    handleInputErrors,
+    AuthController.login
+)
+
+
 export default router
