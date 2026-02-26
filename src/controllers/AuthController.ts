aae7a4bd3d4f330ce7e74aafbbef6efaaac80c56
Milton Munoz <livemilton@gmail.com>
2026-02-25 19:41:56 -0500

    uptask-backend :sparkles: Authentication - Checking if the User is confirmed

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -53,7 +53,7 @@ export class AuthController {
             const tokenExists = await Token.findOne({token})
             if(!tokenExists){
                 const error = new Error('Token no válido')
-                return res.status(401).json({error: error.message})
+                return res.status(404).json({error: error.message})
             }
             
             const user = await User.findById(tokenExists.user)
@@ -73,8 +73,28 @@ export class AuthController {
             const user = await User.findOne({email})
             if(!user){
                 const error = new Error('Usuario no encontrado')
+                return res.status(404).json({error: error.message})
+            }
+
+            if(!user.confirmed){
+                // if user is not confirm, generate a new token and save into DB
+                const token = new Token()
+                token.user = user._id
+                token.token = generateToken()
+                await token.save()
+
+                // send and email
+                AuthEmail.sendConfirmationEmail({
+                    email: user.email,
+                    name: user.name,
+                    token: token.token
+                })
+
+                const error = new Error('La cuenta no ha sido confirmada, hemos enviado un e-mail de confirmación')
                 return res.status(401).json({error: error.message})
             }
+
+
 
         } catch (error) {
             res.status(500).json({error: 'Hubo un error'})
