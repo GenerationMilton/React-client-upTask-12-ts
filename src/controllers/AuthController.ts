2353e9278b1555315ce1c50b90c9958119b2b010
Milton Munoz <livemilton@gmail.com>
2026-03-01 07:40:57 -0500

    uptask-backend :sparkles: Requesting a new confirmation code if it expires

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -108,4 +108,43 @@ export class AuthController {
     
     }
 
+
+       static requestConfirmationCode = async (req: Request, res: Response) => {
+        try {
+
+            //validation to user already registered
+            const { email } = req.body
+
+            // Avoid duplicate - User exists
+            const user = await User.findOne({email})
+            if (!user){
+                const error = new Error('El Usuario no esta registrado')
+                return res.status(404).json({error: error.message})
+            }
+
+            if(user.confirmed){
+                const error = new Error('EL Usuario ya esta confirmado')
+                return res.status(403).json({error: error.message})
+            }
+  
+            // Generate token
+            const token = new Token()
+            token.token = generateToken()
+            token.user = user._id
+
+            // Generar el email
+            AuthEmail.sendConfirmationEmail({
+                email: user.email,
+                name: user.email,
+                token: token.token
+            })
+  
+            await Promise.allSettled([user.save(), token.save()])
+
+            res.send('Se envió un nuevo token a tu e-mail')
+        } catch (error) {
+            res.status(500).json({error: 'Hubo un error'})
+        }
+    }
+
 }
