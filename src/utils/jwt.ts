81704ce65119f26e68c95eb2b5375a5134bfdd1d
Milton Munoz <livemilton@gmail.com>
2026-03-14 21:04:55 -0500

    uptask-backend :sparkles: Generating a Json web token

---
+++ src/utils/jwt.ts
@@ -0,0 +1,15 @@
+import jwt from 'jsonwebtoken'
+
+export const generateJWT = () =>{
+
+    const data = {
+        name: 'Juan',
+        credit_card: '123456543434',
+        password: 'password'
+    }
+
+    const token = jwt.sign(data, process.env.JWT_SECRET,{
+        expiresIn: '6m'
+    })
+    return token
+}
